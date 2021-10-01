15d9d23750805c10849cad0ce59a598ef27c7bdb
Juan Pablo González <90801306+JuanPablo-Gonzalez@users.noreply.github.com>
2021-10-01 11:56:08 +0200

    Add files via upload

---
+++ Ejercicio_Inditex/inditex.php
@@ -0,0 +1,10 @@
+<?php
+
+$datosInditex= array("precio"=>"31.780","variacion"=>"-0.13%","var."=>"-0.04","volumenTitulos"=>"475495","volumenEuros"=>"15027985.74");
+
+foreach($datosInditex as $nombreDato => $valor) {
+    echo $nombreDato . "= " . $valor;
+    echo "<br>";
+}
+
+?>
