cd02c27f128ce630c4ffe957cca375a934776c2c
Juan Pablo González <90801306+JuanPablo-Gonzalez@users.noreply.github.com>
2021-10-21 09:06:19 +0200

    Add files via upload

--- UT2/04_Formularios/calculadora/funcion_calcular.php
+++ UT2/04_Formularios/calculadora/funcion_calcular.php
@@ -1,15 +1,5 @@
 <?php
-    /*Juan Pablo González*/
-
-    //var_dump($_POST);
-
-    echo "<h1>"."CALCULADORA"."</h1>";
-
-    $num1= $_POST["operando1"];
-    $num2= $_POST["operando2"];
-    $operacionElegida= $_POST["operacion"];
-
-    function calcular1() {
+    function calcular($num1,$num2,$operacionElegida) {
         $resultado= 0;
 
         if($operacionElegida=="suma") {
@@ -24,12 +14,11 @@
             $simboloOperacion= "*";
             $resultado= $num1*$num2;
         }
-        else if($operacionElegida=="division") {
+        else if($operacionElegida=="division"){
             $simboloOperacion= "/";
             $resultado= $num1/$num2;
         }
-        
-        echo "<input type='text' name='Operando1'>";
+            
         echo "Resultado operación: $num1 $simboloOperacion $num2 = $resultado";
     }
 ?>
