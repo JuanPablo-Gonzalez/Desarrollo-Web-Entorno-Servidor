c71cdcadad17719a9fad4a2aa79f989fbc605ef3
Juan Pablo González <90801306+JuanPablo-Gonzalez@users.noreply.github.com>
2021-11-05 08:48:26 +0100

    Add files via upload

---
+++ Ejercicio_P01_Dados/p01_JuanPabloGonzalez/funciones_p01dados.php
@@ -0,0 +1,78 @@
+<?php
+    /*Juan Pablo González*/
+
+    function limpiar($variable) {
+        $variable = trim($variable);
+        $variable = stripslashes($variable);
+        $variable = htmlspecialchars($variable);
+
+        return $variable;
+    }
+
+    //Esta función crea los jugadores y mete los dados al mismo tiempo.
+    function crearJugadoresDados(&$arr,$jugador1,$jugador2,$jugador3,$jugador4,$numDados) {
+        $arrJugadores= array($jugador1,$jugador2,$jugador3,$jugador4);
+
+        for($j=0;$j<4;$j++) {
+            $arr["$arrJugadores[$j]"]= array();
+            for($i=0;$i<$numDados;$i++) {
+                $numAleatorio= random_int(1,6);
+                $arr["$arrJugadores[$j]"][$i]= $numAleatorio;
+            } 
+        }
+    }
+
+    //CON ESTA FUNCIÓN CONSEGUIMOS MOSTRAR EL ARRAY DE JUGADORES.
+    function mostrarJugadores($jugadores) {
+        foreach($jugadores as $numeroJugador=> $jugador) {
+            echo "<table border=1>";
+            echo "<tr>";
+            //EN LA PRIMERA CELDA DE CADA FILA IRÁ SU NOMBRE. LUEGO SUS DADOS.
+            echo "<td>$numeroJugador</td>";
+            foreach($jugador as $indiceDado=> $numDado) {
+                $imagenBola= $numDado.".PNG";
+                echo "<td><img src='images/".$imagenBola."' width='40px' border='.5'></td>";
+            }
+            echo "</tr>";
+            echo "</table>";
+            echo "<br>";
+        }
+    }
+
+    //EN ESTA FUNCIÓN LO QUE HAREMOS SERÁ SUMAR LOS DADOS DE CADA JUGADOR, MOSTRAR SU SUMA, Y ADEMÁS
+    //DECIDIR QUIÉNES SON LOS GANADORES O GANADOR.
+    function comprobarGanador($jugadores) {
+        //Lo primero que haremos será obtener el número más alto que salga de sumar los dados de un jugador.
+        $mayorSuma= 0;
+        $contGanadores= 0;
+
+        foreach($jugadores as $numeroJugador=> $jugador) {
+            $sumaDados= 0;
+            foreach($jugador as $indiceDado=> $numDado) {
+                $sumaDados+= $numDado;
+            }
+            //Mostramos las sumas de los dados de cada jugador.
+            echo "Suma dados de ".$numeroJugador."= ".$sumaDados."<br>";
+            if($sumaDados>$mayorSuma) {
+                $mayorSuma= $sumaDados;
+            } 
+        }
+
+        echo "<br>";
+
+        //Luego volveremos a recorrer los jugadores, y si la suma de sus dados coincide con el número
+        //que ha salido, entonces será ganador.
+        foreach($jugadores as $numeroJugador=> $jugador) {
+            $sumaDados= 0;
+            foreach($jugador as $indiceDado=> $numDado) {
+                $sumaDados+= $numDado;
+            }
+            if($sumaDados==$mayorSuma) {
+                echo "GANADOR: $numeroJugador <br>";
+                $contGanadores++;
+            } 
+        }
+
+        echo "<br>NÚMERO GANADORES: $contGanadores";
+    }
+?>
---
+++ Ejercicio_P01_Dados/p01_JuanPabloGonzalez_Mejorado/p01dados.php
@@ -0,0 +1,36 @@
+<?php
+    /*Juan Pablo González*/
+
+    require 'funciones_p01dados.php';
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $nombre1= limpiar($_POST["nombre1"]);
+        $nombre2= limpiar($_POST["nombre2"]);
+        $nombre3= limpiar($_POST["nombre3"]);
+        $nombre4= limpiar($_POST["nombre4"]);
+        $numDados= limpiar($_POST["numdados"]);
+    }
+
+    echo "<h3>RESULTADO JUEGO DADOS</h3>";
+
+    //Si no hay 4 jugadores, o el número de dados es incorrecto, no se ejecutarán las funciones.
+    if($nombre1==""||$nombre2==""||$nombre3==""||$nombre4=="")
+        echo  "Número de jugadores incorrecto";
+    else if($numDados==""||$numDados>10||$numDados<1) {
+        echo "Número de dados incorrecto";
+    } else {
+        //haremos una matriz jugador con los 4 jugadores, cada jugador tendrá un array 
+        //con el número de dados especificado.
+        $jugadores= array();
+
+        //creamos los jugadores y sus dados.
+        crearJugadoresDados($jugadores,$nombre1,$nombre2,$nombre3,$nombre4,$numDados);
+        //var_dump($jugadores);
+
+        //mostramos los jugadores y sus dados en una tabla.
+        mostrarJugadores($jugadores);
+
+        //mostramos la suma de los dados de cada jugador y decidimos ganador.
+        comprobarGanador($jugadores,$numDados); 
+    }
+?>
