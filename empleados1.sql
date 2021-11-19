b89ea3692f6ea21aea3c8a9bd6965683c19a9e13
Juan Pablo González <90801306+JuanPablo-Gonzalez@users.noreply.github.com>
2021-11-19 13:57:39 +0100

    Add files via upload

--- empleados1.sql
+++ empleados1.sql
@@ -22,3 +22,10 @@ pk_nombre_d PRIMARY KEY (nombre_d);
 /*references dice de que tabla y campo viene esa fk*/
 ALTER TABLE empleado ADD CONSTRAINT
 fk_nombre_d FOREIGN KEY (nombre_d) REFERENCES departamento(nombre_d);
+
+/*Damos de alta un departamento, porque para luego meter un departamento en un empleado tiene que estar creado.*/
+INSERT INTO departamento (nombre_d) VALUES ("CONTABILIDAD");
+
+INSERT INTO empleado (dni,nombre_e,fecha_nac,nombre_d) 
+VALUES ("12345678A","Juan Pablo Gonzalez","01-01-21","CONTABILIDAD");
+
