9d2ffd0a5b8facc4a38193503975fcff987664e7
Juan Pablo González <90801306+JuanPablo-Gonzalez@users.noreply.github.com>
2021-11-26 12:56:23 +0100

    Add files via upload

---
+++ Ejercicio_Alta/alta_e_lista.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<?php
+    require 'funciones_alta.php';
+?>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Calculadora</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+</head>
+<body>
+    <form name="formulario" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>"> 
+        <label for="dniEmp">DNI Empleado</label> 
+        <input type="text" name="dniEmp"> <br>
+        <label for="nombreEmp">Nombre Empleado</label> 
+        <input type="text" name="nombreEmp"> <br>
+        <label for="fechaNac">Fecha nacimiento Empleado</label> 
+        <input type="text" name="fechaNac"> <br>
+        <label for="departamentos">Nombre Departamento del empleado</label> 
+        <?php
+           mostrarSelect();
+        ?>
+    
+        <br>
+
+        <input type="submit" value="enviar">
+        <input type="reset" value="borrar">
+    </form>   
+
+    <?php
+		if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $dniEmp= limpiar($_POST["dniEmp"]);
+            $nombreEmp= limpiar($_POST["nombreEmp"]);
+            $fechaNac= limpiar($_POST["fechaNac"]);
+            $nombreDept= limpiar($_POST["departamentos"]);
+            $nombreDept= strtoupper($nombreDept);
+    
+            //var_dump($_POST);
+            altaEmpleadoPDOLista($dniEmp,$nombreEmp,$fechaNac,$nombreDept);
+        }
+	?>
+</body>
+</html>
